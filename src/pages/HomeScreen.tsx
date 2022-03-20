5487df93c3e887210d89557639caced0d067da87
Bruno Cardoso <brunomusic_3@hotmail.com>
2022-03-20 01:56:00 -0300

    update home page

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -59,12 +59,7 @@ function HomeScreen() {
             direction={'row'}
             justify={'flex-end'}
           >
-            <Image
-              src={
-                'https://images.pexels.com/photos/3965545/pexels-photo-3965545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-              }
-              alt={'summer'}
-            />
+
           </Stack>
         </Stack>
         {/* End First */}
@@ -74,69 +69,7 @@ function HomeScreen() {
             Compartilhe a sua vibe
           </Heading>
         </Center>
-        <Text fontWeight={'bold'}>#minhavibe</Text>
-        <Grid
-          h="600px"
-          templateRows={{
-            base: 'repeat(4, 1fr)',
-            md: 'repeat(2, 1fr)'
-          }}
-          templateColumns={{
-            base: 'repeat(1, 1fr)',
-            md: 'repeat(5, 1fr)'
-          }}
-          gap={4}
-          w={'80%'}
-          mb={'5rem'}
-        >
-          <GridItem
-            rowSpan={{
-              base: 1,
-              md: 2
-            }}
-            colSpan={1}
-            bgImage={
-              'https://images.pexels.com/photos/4937224/pexels-photo-4937224.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-            }
-            bgSize={'cover'}
-            bgPosition={'left'}
-          />
-          <GridItem
-            colSpan={{
-              base: 1,
-              md: 2
-            }}
-            rowSpan={{
-              base: 1,
-              md: 1
-            }}
-            bgImage={
-              'https://images.pexels.com/photos/4937222/pexels-photo-4937222.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-            }
-          />
-          <GridItem
-            colSpan={{
-              base: 1,
-              md: 2
-            }}
-            bgImage={
-              'https://images.pexels.com/photos/5704410/pexels-photo-5704410.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-            }
-            bgSize={'100%'}
-            bgPosition={'center'}
-          />
-          <GridItem
-            colSpan={{
-              base: 1,
-              md: 4
-            }}
-            bgImage={
-              'https://images.pexels.com/photos/291762/pexels-photo-291762.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-            }
-            bgSize={'100%'}
-            bgPosition={'center'}
-          />
-        </Grid>
+
         {/* End Grid */}
         <NewsLetter />
         {/* Best sellers  */}
